e70d01c8744eb9d165535566accf611d25691fc3
Mark Hoffner <mhoffner@theride-edp.com>
2021-12-30 10:30:45 -0500

    Add date display

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <div>Hello {value}</div>
+    <div>The time is now {Date()}.</div>
+  </div>
+
 }
 
 export default App;
